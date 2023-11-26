1e74dab44b75f24e0ea15f85548b5ac344b1b7b4
sailendrasah <sailendrasah251920@gmail.com>
2023-11-26 11:31:19 +0545

    tax file added

---
+++ employe.c
@@ -0,0 +1,36 @@
+ #include<stdio.h>
+ #include<string.h>
+
+ struct employe{
+
+
+int code;
+float salary;
+char name[20];
+};
+
+int main (){
+struct employe e1,e2, e3;
+printf("enter the value of e1:");
+scanf("%d",&e1.code);
+printf("enter the value of e1:");
+scanf("%f",&e1.salary);
+printf("enter the value of e1:");
+scanf("%s",e1.name);
+printf("enter the value of e2:");
+scanf("%d",&e2.code);
+printf("enter the value of e2:");
+scanf("%f",&e2.salary);
+printf("enter the value of e2:");
+scanf("%s",e2.name);
+
+printf("enter the value of e3:");
+scanf("%f",&e3.salary);
+printf("enter the value of e3:");
+scanf("%s",&e3.name);
+printf("entr the value of e3:");
+scanf("%s",e3.name);
+
+
+
+}
