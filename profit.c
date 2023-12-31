9ad6f10148e30d1becbb3b69f276754825ec619f
sailendrasah <sailendrasah251920@gmail.com>
2023-12-31 12:40:31 +0545

    happy newyear file added

--- profit.c
+++ profit.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+/*
 int main(){
     int cp;
     int sp;
@@ -14,4 +15,22 @@ else{
 }
 int profit=cp-sp;
 printf("%d\n",profit);
+}*/
+int main(){
+    int num,reverse=0,remainder;
+    printf("enter the num");
+    scanf("%d",&num);
+if(num!=0){
+num=(num/10)%10;
+reverse=num*0+num;
+
+num=(num/100)%10;
+reverse=num*0+num;
+printf("%d",reverse);
+// num=(num/100)%10;
+// reverse=num*0+num;
+// printf("%d",reverse);
+
+
+}
 }
