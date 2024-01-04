6c23d135282f0fe608c9fccba89ccc5989aa206c
sailendrasah <sailendrasah251920@gmail.com>
2024-01-04 06:55:14 +0545

    manoj file added

---
+++ oddeven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("enter the value");
+    scanf("%d",&x);
+    if(x%2==0){
+        printf("even number");
+    }
+    else{
+        printf("odd number");
+
+    }
+}
