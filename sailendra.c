ba1ec6e097157c20cbb59ec6004da91eb68ae86c
sailendrasah <sailendrasah251920@gmail.com>
2024-01-04 08:52:14 +0545

    sailendra file added

---
+++ sailendra.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int ram,shyam,ajay;
+    printf("enter the age of ram");
+    scanf("%d",&ram);
+    printf("enter the age of shyam");
+    scanf("%d",&shyam);
+    printf("enter the age of a");
+    scanf("%d",&ajay);
+if(ram<shyam){
+    printf("ram is yongest");
+}
+if(shyam<ajay){
+    printf("shyam is yongest");
+}
+if(ajay<ram){
+    printf("ajay is youngest");
+}
+
+    
+
+}
