d6ebe0a84c4ffbb32cffda416f23a698e1587679
sailendrasah <sailendrasah251920@gmail.com>
2024-01-10 07:47:09 +0545

    sailendra file added

---
+++ even.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,number=2;
+    printf("enter the n");
+    scanf("%d",&n);
+    while(number<=n){
+    printf("%d\n",number);
+    number=number+2;
+    }
+// printf("%d",number);
+}
---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num,z,result=1;
+    printf("enter the value");
+    scanf("%d %d",&num,&z);
+    while(num){
+     result*num;
+    }
+    printf("%d",num,z,result);
+}
