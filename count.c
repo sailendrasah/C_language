40d2028f92e023675bf1e135a2327131feca532a
sailendrasah <sailendrasah251920@gmail.com>
2024-01-11 06:56:41 +0545

    sailendra file added

---
+++ count.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n,ne=0,p=0,z=0;
+    // printf("enter the value");
+    // scanf("%d",&n);
+    while(n){
+    printf("enter the value");
+
+    scanf("%d",&n);
+if(n>0){
+p++;
+}
+else if(n<0){
+ne++;
+}
+else if(n==0){
+z++;
+}
+// n-1;
+    
+    printf("positive number is %d\n",p); 
+    printf("negative number is %d\n",ne); 
+    printf("zero number is %d\n",z); 
+
+    
+    }
+}
